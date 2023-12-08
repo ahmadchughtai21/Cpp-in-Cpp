828722ab7f132fbe44f0dd1e4f94c6098f46d09c
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2023-12-09 02:30:10 +0500

    1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Alphabet A in Cpp
+ A C++ program to print the alphabet A (using escape sequence) on the console
---
+++ task5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+ using namespace std;
+int main() {
+cout<<"cccccccc                 ++                     ++            \n";
+cout<<"cc                       ++                     ++            \n";
+cout<<"cc                  +++++++++++++          ++++++++++++       \n";
+cout<<"cc                  +++++++++++++          ++++++++++++       \n";
+cout<<"cc                       ++                     ++            \n";
+cout<<"cccccccc                 ++                     ++            \n";
+return 0;
+}
